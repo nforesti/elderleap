76fe90e7e31dfb538b2dce597eb32e995b09a2e4
nforesti <nforesti@ucsd.edu>
2019-04-26 17:01:21 -0700

    got voice working for subcategories

--- static_files/js/speech.js
+++ static_files/js/speech.js
@@ -2,12 +2,11 @@
 original code by Chris Mills
 https://github.com/mdn/web-speech-api/blob/master/speak-easy-synthesis
 */
-function toSpeech() {
-    console.log(this);
+function toSpeech(word) {
     /*
     event.stopPropagation();
     event.stopImmediatePropagation();*/
-    //(... rest of your JS code) 
+
 
     const synth = window.speechSynthesis;
     const voices = window.speechSynthesis.getVoices()
@@ -16,21 +15,18 @@ function toSpeech() {
         console.error('speechSynthesis.speaking');
         return;
     }
-    let text = $(this).children[1].value;
-    var utterThis = new SpeechSynthesisUtterance(text);
-    utterThis.onend = function (event) {
+    const utterThis = new SpeechSynthesisUtterance(word);
+    /*utterThis.onend = function (event) {
         console.log('SpeechSynthesisUtterance.onend');
-    }
+    }*/
     utterThis.onerror = function (event) {
         console.error('SpeechSynthesisUtterance.onerror');
     }
 
-    utterThis.voice = voices[12];
+
+    utterThis.lang = "en-US"
     utterThis.pitch = .7;
     utterThis.rate = .9;
     synth.speak(utterThis);
     console.log("isspeaking");
-
-
-
 }
