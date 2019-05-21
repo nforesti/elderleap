46e4d2adf6359518ca42b014b94960ea821dc49f
nforesti <nforesti@ucsd.edu>
2019-05-21 10:19:18 -0700

    line up top of categories and subcategories

--- static_files/js/browse.js
+++ static_files/js/browse.js
@@ -15,19 +15,27 @@ $(document).ready(() => {
                 $('#subTitle').html("");
                 console.log('You received some data!', data);
                 let html = "";
+                let counter = 0;
                 for (category in data) {
-                    html = html + '<li class="flex-item topcategory category">\n<img src="' + data[category].img.img + '">\n<h3>' + category + '</h3>\n</li>';
+                    if (counter != 0) {
+                        html = html + '<li class="flex-item topcategory category">\n<img src="' + data[category].img.img + '">\n<h3>' + category + '</h3>\n</li>';
+                    }
+                    else {
+                        html = html + '<li style="margin-top: 0px !important;" class="flex-item topcategory category">\n<img src="' + data[category].img.img + '">\n<h3>' + category + '</h3>\n</li>';
+
+                    }
                     console.log(html);
                     $('#categoryContainer').html(html);
                     console.log(data[category].img);
+                    counter++;
                 }
             },
         });
     }
-    $("#backToHomeBtn").click((e) => {enterBrowse();});
+    $("#backToHomeBtn").click((e) => { enterBrowse(); });
     enterBrowse();
 
-    $("#updateDatabase").click((e) => {updateDatabase();});
+    $("#updateDatabase").click((e) => { updateDatabase(); });
 
     $(document).on('click', '.topcategory', function () {
         const catName = $(this).find("h3")[0].innerHTML;
@@ -39,22 +47,29 @@ $(document).ready(() => {
             success: (data) => {
                 $('#subTitle').html(catName);
                 let html = "";
+                let counter = 0;
                 for (subcategory in data) {
                     if (subcategory != "img") {
-                        html = html + '<li onclick="toSpeech(&quot;' + subcategory + '&quot;)" class="flex-item category">\n<img src="' + data[subcategory].img.img + '">\n<h3 class="subcategoryName">' + subcategory + '</h3>\n</li>';
+                        if (counter != 0) {
+                            html = html + '<li onclick="toSpeech(&quot;' + subcategory + '&quot;)" class="flex-item category">\n<img src="' + data[subcategory].img.img + '">\n<h3 class="subcategoryName">' + subcategory + '</h3>\n</li>';
+                        }
+                        else {
+                            html = html + '<li style="margin-top: 0px !important;" onclick="toSpeech(&quot;' + subcategory + '&quot;)" class="flex-item category">\n<img src="' + data[subcategory].img.img + '">\n<h3 class="subcategoryName">' + subcategory + '</h3>\n</li>';
+                        }
                         $('#categoryContainer').html(html);
                     }
+                    counter++;
                 }
             },
         });
         $.ajax({
-            url: '/sentences/'+catName,
+            url: '/sentences/' + catName,
             type: 'GET',
             dataType: 'json',
             success: (data) => {
                 html = "";
-                data.words.forEach(word =>{
-                    html+='<div onclick="toSpeech(&quot;' + word + '&quot;)"class="sentence btn btn-small">'+word+'</div>';
+                data.words.forEach(word => {
+                    html += '<div onclick="toSpeech(&quot;' + word + '&quot;)"class="sentence btn btn-small">' + word + '</div>';
                 });
                 $('#sentences').html(html);
             },
@@ -63,5 +78,5 @@ $(document).ready(() => {
         document.getElementById("suggestionContainer").style.display = "inline";
         document.getElementById("backToHomeBtn").style.display = "inline";
     });
-    
+
 });
