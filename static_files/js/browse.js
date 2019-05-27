e66e8d18cf5981fc77bd8932ee229d5b2a7675d1
nforesti <nforesti@ucsd.edu>
2019-05-27 15:48:27 -0700

    speak and drag bugs fixed

--- static_files/js/browse.js
+++ static_files/js/browse.js
@@ -18,6 +18,7 @@ $(document).ready(() => {
                 document.getElementById("dropDiv").style.display = "none";
                 $("#dropDiv").empty(); 
                 document.getElementById("clearBtn").style.display ="none";
+                document.getElementById("speakBtn").style.display ="none";
                 console.log('You received some data!', data);
                 let html = "";
                 let counter = 0;
@@ -55,10 +56,10 @@ $(document).ready(() => {
                 for (subcategory in data) {
                     if (subcategory != "img") {
                         if (counter != 0) {
-                            html = html + '<li onclick="toSpeech(&quot;' + subcategory + '&quot;)" id="target '+ subcategory +'" class="flex-item category">\n<img draggable="true" ondragstart="drag(event)" id="'+subcategory+'" src="' + data[subcategory].img.img + '">\n<h3 class="subcategoryName">' + subcategory + '</h3>\n</li>';
+                            html = html + '<li onclick="toSpeech(&quot;' + subcategory + '&quot;)" id="target '+ subcategory +'" class="flex-item category">\n<img draggable="true" ondragstart="drag(event)" id="'+subcategory+'" src="' + data[subcategory].img.img + '">\n <div class="text-block" draggable="true"  ondragstart="drag($(this).prev("img")[0])" align="left"><i class="fas fa-expand-arrows-alt"></i></div><h3 class="subcategoryName">' + subcategory + '</h3>\n</li>';
                         }
                         else {
-                            html = html + '<li style="margin-top: 0px !important;" onclick="toSpeech(&quot;' + subcategory + '&quot;)"id="target '+ subcategory +'" class="flex-item category" >\n<img  draggable="true" ondragstart="drag(event)" id="'+subcategory+'" src="' + data[subcategory].img.img + '">\n<h3 class="subcategoryName">' + subcategory + '</h3>\n</li>';
+                            html = html + '<li style="margin-top: 0px !important;" onclick="toSpeech(&quot;' + subcategory + '&quot;)"id="target '+ subcategory +'" class="flex-item category" >\n<img  draggable="true" ondragstart="drag(event)" id="'+subcategory+'" src="' + data[subcategory].img.img + '">\n <div class="text-block" draggable="true" ondragstart="drag($(this).prev("img")[0])" align="left"><i  class="fas fa-expand-arrows-alt"></i></div>\n<h3 class="subcategoryName">' + subcategory + '</h3>\n</li>';
                         }
                         $('#categoryContainer').html(html);
                     }
@@ -73,7 +74,7 @@ $(document).ready(() => {
             success: (data) => {
                 html = "";
                 data.words.forEach(word => {
-                    html += '<div onclick="toSpeech(&quot;' + word + '&quot;)" class="sentence btn btn-small" id= "'+ word +'" ondragstart="dragStart(event)" draggable="true">' + word + '</div>';
+                    html += '<div onclick="toSpeech(&quot;' + word + '&quot;)" class="sentence btn btn-small" id= "'+ word +'" ondragstart="dragStart(event)" draggable="true"><div class="text-block-sentence" align="center"><i style="cursor: grab" class="fas fa-expand-arrows-alt"></i></div>' + word + '</div>';
                 });
                 $('#sentences').html(html);
             },
@@ -86,6 +87,7 @@ $(document).ready(() => {
         document.getElementById("subTitle").style.display="block";
         document.getElementById("subTitle_description").style.display="block";
         document.getElementById("clearBtn").style.display ="inline";
+        document.getElementById("speakBtn").style.display ="inline";
     });
 
 });
--- static_files/js/dragdrop.js
+++ static_files/js/dragdrop.js
@@ -1,13 +1,22 @@
 
 function clearDropDiv() {
-    $("#dropDiv").empty(); 
+    $("#dropDiv").empty();
 }
 
 function allowDrop(ev) {
-    ev.preventDefault();
+     ev.preventDefault();
 }
 
 function drag(ev) {
+    console.log("drag");
+
+    console.log(event.target.id);
+    if ($(event.target).hasClass("text-block")) {
+        console.log(event.target);
+        console.log($(event.target).prev("img"));
+        event.target.id = $(event.target).prev("img")[0].id;
+        console.log("newest" + event.target.id);
+    }
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
@@ -15,7 +24,10 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     var nodeCopy = document.getElementById(data).cloneNode(true);
-    nodeCopy.id = "new" + data ; 
+    nodeCopy.id = "new" + data;
+    let newtext = nodeCopy.textContent;
+    $(nodeCopy).empty();
+    nodeCopy.textContent=newtext;
     ev.target.appendChild(nodeCopy);
     document.getElementById(nodeCopy.id).setAttribute('draggable', false);
     document.getElementById(nodeCopy.id).removeAttribute("onclick");
@@ -27,10 +39,29 @@ function dragEnd(event) {
 */
 
 function dragStart(event) {
+
+    if ($(event.target).hasClass("text-block")) {
+        console.log(event.target);
+        console.log($(event.target).prev("img"));
+        event.target.id = $(event.target).prev("img")[0].id;
+        console.log("newest" + event.target.id);
+    }
+    console.log("dragStart");
     event.dataTransfer.setData("Text", event.target.id);
-    
-  }
 
+}
 
-
+function readAloud(event) {
+    let dragString = "";
+    document.getElementById('dropDiv').querySelectorAll('*').forEach(node => {
+        if ($(node).hasClass("sentence")){
+            dragString += node.textContent;
+        }
+        else{
+        dragString += node.id.replace('new', ' ') + " ";
+    }
+});
+console.log(dragString);
+toSpeech(dragString);
+}
 
