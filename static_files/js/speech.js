4fe22bd1413402590af10c50ff671a3fa5ab765c
nforesti <nforesti@ucsd.edu>
2019-05-29 17:33:09 -0700

    speech.js

--- static_files/js/speech.js
+++ static_files/js/speech.js
@@ -1,22 +1,21 @@
-/* 
-original code by Chris Mills
-https://github.com/mdn/web-speech-api/blob/master/speak-easy-synthesis
-*/
+//Using DOM SpeechSynthesis, reads aloud a string
 function toSpeech(word) {
     let synth = window.speechSynthesis;
     var msg = new SpeechSynthesisUtterance(word);
 
+    //get voices of user's browser
     let voices = synth.getVoices();
     for (let voice of voices) {
-        console.log("there");
         if ((voice.lang === 'en-US') && (voice.name === '"Microsoft Zira Desktop - English (United States)"')) {
             msg.voice = voice;
         }
     }
+    //set characteristics of voice
     msg.rate = .7;
     msg.lang = "en-US";
     synth.speak(msg);
-
+    
+    //message if any error occurs
     msg.onerror = function (event) {
         console.error('SpeechSynthesisUtterance.onerror');
     }
